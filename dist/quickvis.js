efe8d30444c834d70620938780c44e98e7959dd5
John <rDr4g0n@gmail.com>
2016-08-09 09:46:17 -0500

    bump built js lib from 0.1.2 to 0.1.3-dev

--- dist/quickvis.js
+++ dist/quickvis.js
@@ -8,7 +8,7 @@
 })();
     
 
-/* QuickVis v0.1.2 */
+/* QuickVis v0.1.3-dev */
 var quickvis = (function () {
   'use strict';
 
@@ -780,4 +780,4 @@ var quickvis = (function () {
 
 }());
 
-//# sourceMappingURL=quickvis-0.1.2.js.map
+//# sourceMappingURL=quickvis-0.1.3-dev.js.map
